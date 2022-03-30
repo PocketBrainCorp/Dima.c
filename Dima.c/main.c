076276e9c485c05991382846e996188f9d6e1cd0
PocketBrain <pocketbaincorp@gmail.com>
2022-03-30 14:35:55 +0300

    Initial Commit

---
+++ Dima.c/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Dima.c
+//
+//  Created by Владислав on 30.03.2022.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
